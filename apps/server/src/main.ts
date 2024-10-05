4c8befe30f1299009e0a7b8ca349ba1cf9fca6a2
Eugene Pobochny <eugene.pobochny@gmail.com>
2024-10-05 01:21:38 -0400

    chore: tsx

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Express on Vercel");
+  res.send("Hello world");
 });
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
