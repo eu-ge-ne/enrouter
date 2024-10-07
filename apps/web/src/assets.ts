011003eeee56d22858dd209f07d870fe6db41d5f
Eugene Pobochny <eugene.pobochny@gmail.com>
2024-10-07 18:55:40 -0400

    1

---
+++ apps/web/src/assets.ts
@@ -0,0 +1,46 @@
+import type { ModuleAssets } from "enrouter";
+
+import { createLog } from "./log.js";
+
+//@ts-ignore
+import manifest from "@enrouter/web/manifest";
+
+const log = createLog("assets");
+
+type ManifestItem = {
+  src?: string;
+  file: string;
+  css?: string[];
+  assets?: string[];
+  isEntry?: boolean;
+  isDynamicEntry?: boolean;
+  imports?: string[];
+  dynamicImports?: string[];
+};
+
+log("Building assets");
+
+export const assets: ModuleAssets = Object.fromEntries(
+  Object.entries<ManifestItem>(manifest).map(([key, val]) => {
+    const imports = new Set<string>();
+    flattenImports(val.imports ?? [], imports);
+    return [
+      key,
+      {
+        styles: val.css ?? [],
+        modules: [val.file, ...imports],
+      },
+    ];
+  })
+);
+
+log("Assets built: %O", assets);
+
+function flattenImports(ids: string[], result: Set<string>) {
+  for (const id of ids) {
+    const item = (manifest as Record<string, ManifestItem>)[id]!;
+    result.add(item.file);
+    const importIds = item.imports ?? [];
+    flattenImports(importIds, result);
+  }
+}
