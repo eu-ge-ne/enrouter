81da23137b82e201067fa238c8844bdd594bbef3
Eugene Pobochny <eugene.pobochny@gmail.com>
2024-10-06 22:44:23 -0400

    tests

--- libs/enrouter/src/routes/mod.unit.test.ts
+++ libs/enrouter/src/routes/mod.unit.test.ts
@@ -14,4 +14,38 @@ describe("buildRoutes", () => {
 
     expect(buildRoutes(params)).toMatchSnapshot();
   });
+
+  test("from 1 module", () => {
+    const params: BuildRoutesParams = {
+      entryId: "src/main.tsx",
+      getModuleAssets: (moduleId) => ({ modules: [moduleId], styles: [] }),
+      modules: {
+        "src/_layout.tsx": {
+          path: "_layout.tsx",
+          load: async () => undefined,
+        },
+      },
+    };
+
+    expect(buildRoutes(params)).toMatchSnapshot();
+  });
+
+  test("from 2 parent-child modules", () => {
+    const params: BuildRoutesParams = {
+      entryId: "src/main.tsx",
+      getModuleAssets: (moduleId) => ({ modules: [moduleId], styles: [] }),
+      modules: {
+        "src/_layout.tsx": {
+          path: "_layout.tsx",
+          load: async () => undefined,
+        },
+        "src/abc/_layout.tsx": {
+          path: "abc/_layout.tsx",
+          load: async () => undefined,
+        },
+      },
+    };
+
+    expect(buildRoutes(params)).toMatchSnapshot();
+  });
 });
