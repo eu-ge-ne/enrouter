3cddbf9073bc38a509102dd50d4aba2890353afa
Eugene Pobochny <eugene.pobochny@gmail.com>
2024-10-07 13:46:25 -0400

    1

---
+++ apps/web/src/routes.ts
@@ -0,0 +1,10 @@
+import type { RouteModules } from "enrouter";
+
+const _modules = import.meta.glob(["./app/**/_*.tsx"]);
+
+export const modules: RouteModules = Object.fromEntries(
+  Object.entries(_modules).map(([key, load]) => [
+    "src" + key.slice(".".length),
+    { path: key.slice("./app/".length), load },
+  ])
+);
