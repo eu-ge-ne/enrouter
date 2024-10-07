d8f346c708391226678078c9787b86326beb9842
Eugene Pobochny <eugene.pobochny@gmail.com>
2024-10-07 18:55:05 -0400

    1

---
+++ apps/web/src/log.ts
@@ -0,0 +1,13 @@
+export function createLog(ns: string): typeof console.log {
+  let t = Date.now();
+
+  const log: typeof console.log = (msg, ...params) => {
+    console.log(
+      `@enrouter/web/${ns} (+${Date.now() - t}ms): ${msg}`,
+      ...params
+    );
+    t = Date.now();
+  };
+
+  return log;
+}
