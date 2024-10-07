ffd6451afd2643f5fb8966a6d559db754d299588
Eugene Pobochny <eugene.pobochny@gmail.com>
2024-10-07 02:31:36 -0400

    feat: hooks

---
+++ libs/enrouter/src/hooks.ts
@@ -0,0 +1,25 @@
+import { useContext } from "react";
+
+import { RouterContext } from "./routers/context.js";
+import { RouteRenderContext } from "./render/context.js";
+
+import type { RouteMatch } from "./matches/mod.js";
+
+export function useLocation(): string {
+  const { location } = useContext(RouterContext);
+  return location;
+}
+
+export function usePath(path: string): RouteMatch | undefined {
+  let nodes = useContext(RouteRenderContext).last;
+  while (nodes) {
+    if (nodes.match.handler.route.path === path) {
+      return nodes.match;
+    }
+    nodes = nodes.prev;
+  }
+}
+
+export function useMatch(): RouteMatch {
+  return useContext(RouteRenderContext).match;
+}
