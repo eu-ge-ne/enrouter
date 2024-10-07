2bc16599b806c1c900c35efe6da7583b4982bae2
Eugene Pobochny <eu-ge-ne@users.noreply.github.com>
2024-10-07 02:25:55 -0400

    feat: outlets (#23)

---
+++ libs/enrouter/src/outlets/mod.ts
@@ -0,0 +1,28 @@
+import { useContext } from "react";
+import type { ReactNode, ReactElement } from "react";
+
+import { RouteRenderContext } from "#render/context.js";
+
+interface OutletProps {
+  name: string;
+}
+
+export function Outlet({ name }: OutletProps): ReactNode {
+  return useOutlets()?.[name];
+}
+
+export function useOutlets(): Record<string, ReactElement> | undefined {
+  let nodes = useContext(RouteRenderContext);
+
+  if (!nodes.next) {
+    return nodes.index;
+  }
+
+  while (nodes.next) {
+    nodes = nodes.next;
+    const outlets = nodes.layout || nodes.index;
+    if (outlets) {
+      return outlets;
+    }
+  }
+}
