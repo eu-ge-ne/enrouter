7f2cab480d41c7435e03fdb1d1de8dea697c36fd
Eugene Pobochny <eu-ge-ne@users.noreply.github.com>
2024-10-06 21:06:55 -0400

    feat: npm link (#16)

--- apps/web/src/app.tsx
+++ apps/web/src/app.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 import { hello } from "enrouter";
 
+import npm_logo from "/npm.svg";
 import github_logo from "/github-light.svg";
 
 export function App() {
@@ -13,17 +14,26 @@ export function App() {
 
   return (
     <div>
-      <div className="p-4 border-b flex gap-x-4 items-center justify-between">
+      <div className="p-4 border-b flex items-center justify-between">
         <span className="text-lg tracking-tighter font-medium font-mono">
           enrouter
         </span>
-        <a href="https://github.com/eu-ge-ne/enrouter" target="_blank">
-          <img
-            src={github_logo}
-            className="size-6"
-            alt="https://github.com/eu-ge-ne/enrouter"
-          />
-        </a>
+        <div className="flex justify-end gap-x-4">
+          <a href="https://www.npmjs.com/package/enrouter" target="_blank">
+            <img
+              src={npm_logo}
+              className="size-6"
+              alt="https://www.npmjs.com/package/enrouter"
+            />
+          </a>
+          <a href="https://github.com/eu-ge-ne/enrouter" target="_blank">
+            <img
+              src={github_logo}
+              className="size-6"
+              alt="https://github.com/eu-ge-ne/enrouter"
+            />
+          </a>
+        </div>
       </div>
       {hello()}
       <button onClick={handleClick}>Count: {count}</button>
