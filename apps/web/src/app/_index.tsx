d4a80eedc3cb67490d4a153aea4d417c9ebf4bcd
Eugene Pobochny <eugene.pobochny@gmail.com>
2024-10-10 20:29:38 -0400

    fix: home

--- apps/web/src/app/_index.tsx
+++ apps/web/src/app/_index.tsx
@@ -1,3 +1,5 @@
+import { useLinkProps } from "enrouter";
+
 import { createLog } from "#log.js";
 
 const log = createLog("_index");
@@ -9,5 +11,29 @@ export const components = {
 function Index() {
   log("Rendering");
 
-  return "TODO";
+  return (
+    <div className="fixed bottom-0 left-0 right-0 top-0 -z-10 flex flex-col items-center justify-center gap-8 p-4">
+      <div className="max-w-[30rem] text-center">
+        <p>enrouter is not ready for use in production yet.</p>
+        <p>
+          It is just making its first baby steps at early development stages.
+        </p>
+      </div>
+      <div className="flex justify-center gap-8">
+        <a
+          className="rounded-md border border-paperBorder px-4 py-2 text-sm"
+          {...useLinkProps("/docs/start")}
+        >
+          Get started
+        </a>
+        <a
+          className="rounded-md border border-paperBorder px-4 py-2 text-sm"
+          href="https://github.com/eu-ge-ne/enrouter"
+          target="_blank"
+        >
+          GitHub
+        </a>
+      </div>
+    </div>
+  );
 }
