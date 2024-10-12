1c543d2a8151fa36099aff46f2b8ee5ca7532c5f
Eugene Pobochny <eugene.pobochny@gmail.com>
2024-10-12 17:21:59 -0400

    1

--- apps/web/src/app/docs/api/route/_layout.tsx
+++ apps/web/src/app/docs/api/route/_layout.tsx
@@ -1,14 +1,9 @@
-import { createLog } from "#log.js";
 import Index from "./index.md";
-
-const log = createLog("docs/api/route/_layout");
 
 export const components = {
   api: Layout,
 };
 
 function Layout() {
-  log("Rendering");
-
   return <Index />;
 }
