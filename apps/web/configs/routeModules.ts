390b64cfd7c88bd0c3be16a4b17283977d12c843
Eugene Pobochny <eugene.pobochny@gmail.com>
2024-10-13 03:29:04 -0400

    1

--- apps/web/configs/routeModules.ts
+++ apps/web/configs/routeModules.ts
@@ -15,7 +15,7 @@ export function routeModules({ routeModulesPath }: RouteModulesParams): Plugin {
   let pathRoot: string;
 
   return {
-    name: "rollup-plugin-route-modules",
+    name: "vite-plugin-route-modules",
     configResolved(config) {
       pathRoot = config.root;
     },
@@ -32,18 +32,25 @@ export function routeModules({ routeModulesPath }: RouteModulesParams): Plugin {
       const pathPrefix = path.resolve(routeModulesPath);
 
       const files = await glob(path.resolve(pathPrefix, "**/_*.tsx"));
+      const resolves = await Promise.all(files.map((x) => this.resolve(x)));
 
-      const mods = await Promise.all(
-        files.map((file) =>
-          this.resolve(file).then((resolved) => [file, resolved!.id] as const),
-        ),
-      );
+      const modules = resolves
+        .map((x, i) =>
+          x
+            ? {
+                key: files[i]!.slice(pathRoot.length + 1),
+                path: files[i]!.slice(pathPrefix.length + 1),
+                id: x.id,
+              }
+            : undefined,
+        )
+        .filter((x) => x !== undefined);
 
-      const modules = mods
+      const str = modules
         .map(
-          ([file, id]) => `
-"${file.slice(pathRoot.length + 1)}": {
-  path: "${file.slice(pathPrefix.length + 1)}",
+          ({ key, path, id }) => `
+"${key}": {
+  path: "${path}",
   load: () => import("${id}"),
 },
 `,
@@ -52,52 +59,9 @@ export function routeModules({ routeModulesPath }: RouteModulesParams): Plugin {
 
       return `
 export const modules = {
-  ${modules}
+  ${str}
 };
 `;
     },
   };
 }
-
-/*
-      const ids = this.getModuleIds();
-      //console.dir({ a, b }, { depth: null });
-      for (const id of ids) {
-        if (id.endsWith("_layout.tsx") || id.endsWith("main.tsx")) {
-          const info = this.getModuleInfo(id);
-          const imports = info?.importedIds;
-          const impRes = info?.importedIdResolutions;
-          console.dir({ id, imports, impRes, info });
-        }
-      }
-      return null;
-      */
-
-/*
-      Hello from virtual module
-      {
-        modules: {
-          'src/app/_notFound.tsx': { path: '_notFound.tsx', load: [Function: src/app/_notFound.tsx] },
-          'src/app/_layout.tsx': { path: '_layout.tsx', load: [Function: src/app/_layout.tsx] },
-          'src/app/_index.tsx': { path: '_index.tsx', load: [Function: src/app/_index.tsx] },
-          'src/app/docs/_notFound.tsx': {
-            path: 'docs/_notFound.tsx',
-            load: [Function: src/app/docs/_notFound.tsx]
-          },
-          'src/app/docs/_layout.tsx': {
-            path: 'docs/_layout.tsx',
-            load: [Function: src/app/docs/_layout.tsx]
-          },
-          'src/app/docs/_index.tsx': {
-            path: 'docs/_index.tsx',
-            load: [Function: src/app/docs/_index.tsx]
-          },
-          'src/app/docs/start/_layout.tsx': {
-            path: 'docs/start/_layout.tsx',
-            load: [Function: src/app/docs/start/_layout.tsx]
-          },
-          'src/app/docs/features/_layout.tsx': {
-            path: 'docs/features/_layout.tsx',
-            load: [Function: src/app/docs/features/_layout.tsx]
-          },
-*/
