5d1101793687f731f47f130f7486e4b1ae2e41b5
Eugene Pobochny <eugene.pobochny@gmail.com>
2024-10-13 21:08:54 -0400

    1

--- libs/enrouter/configs/vitest.workspace.ts
+++ libs/enrouter/configs/vitest.workspace.ts
@@ -3,7 +3,10 @@ import { defineWorkspace } from "vitest/config";
 export default defineWorkspace([
   {
     test: {
-      include: ["lib/src/**/*.unit.test.{ts,tsx}"],
+      include: [
+        "lib/src/**/*.unit.test.{ts,tsx}",
+        "vite/src/**/*.unit.test.{ts,tsx}",
+      ],
       name: "unit",
       environment: "node",
     },
