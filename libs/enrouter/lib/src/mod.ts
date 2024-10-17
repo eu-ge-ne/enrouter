b0131655c8e1e33f19c3ebfd409be9d53a12b075
Eugene Pobochny <eugene.pobochny@gmail.com>
2024-10-17 15:24:24 -0400

    1

--- libs/enrouter/lib/src/mod.ts
+++ libs/enrouter/lib/src/mod.ts
@@ -2,17 +2,11 @@ export { debug } from "./debug.js";
 
 export type { Route } from "./route/mod.js";
 
-export { loadRoutes } from "./route/load.js";
-
 export type { Match } from "./match/mod.js";
 export { type MatchRoutesParams, matchRoutes } from "./match/match.js";
-
-export {
-  type UseActiveParams,
-  useMatch,
-  usePath,
-  useActive,
-} from "./content/hooks.js";
+export { renderMatches } from "./match/render.js";
+export { useMatch } from "./match/context.js";
+export { type UseActiveParams, usePath, useActive } from "./match/hooks.js";
 
 export { type StaticRouterProps, StaticRouter } from "./router/static.js";
 export { type BrowserRouterProps, BrowserRouter } from "./router/browser.js";
