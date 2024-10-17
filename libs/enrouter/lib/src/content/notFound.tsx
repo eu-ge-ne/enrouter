6f06c495884253dd780f44fdf013c034efb6f6a3
Eugene Pobochny <eugene.pobochny@gmail.com>
2024-10-17 15:23:59 -0400

    2

--- libs/enrouter/lib/src/content/notFound.tsx
+++
@@ -1,9 +0,0 @@
-import type { ReactNode } from "react";
-
-import { useLocation } from "#lib/router/hooks.js";
-
-export function NotFound(): ReactNode {
-  const location = useLocation();
-
-  return `${location} is not found`;
-}
