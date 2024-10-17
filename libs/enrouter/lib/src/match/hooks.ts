3256994b035a549c4e24468c35fa73690b697644
Eugene Pobochny <eugene.pobochny@gmail.com>
2024-10-17 15:24:52 -0400

    1

---
+++ libs/enrouter/lib/src/match/hooks.ts
@@ -0,0 +1,27 @@
+import type { Match } from "./mod.js";
+import { useMatch } from "./context.js";
+
+export function usePath(path: string): Match | undefined {
+  let match = useMatch().fist;
+
+  while (match) {
+    if (match.route.path === path) {
+      return match;
+    }
+    match = match.next;
+  }
+}
+
+export interface UseActiveParams<T> {
+  path: string;
+  loose?: boolean;
+  value: [T, T];
+}
+
+export function useActive<T>({ path, loose, value }: UseActiveParams<T>): T {
+  const match = usePath(path);
+
+  const isActive = loose ? match !== undefined : Boolean(match?.isFull);
+
+  return isActive ? value[0] : value[1];
+}
