4a7db9053fa72171430529be0ffa3c02ea989fa0
Eugene Pobochny <eugene.pobochny@gmail.com>
2024-10-17 15:22:14 -0400

    1

--- libs/enrouter/lib/src/router/static.tsx
+++ libs/enrouter/lib/src/router/static.tsx
@@ -3,7 +3,6 @@ import type { ReactNode } from "react";
 import type { Route } from "#lib/route/mod.js";
 import type { Match } from "#lib/match/mod.js";
 import type { TRouterContext } from "./context.js";
-import { createContent } from "#lib/content/create.js";
 import { RouterProvider } from "./context.js";
 
 export interface StaticRouterProps {
@@ -26,7 +25,7 @@ export function StaticRouter({
     ctx,
   };
 
-  const children = createContent(matches);
+  const children = Object.values(matches[0]?.elements?.layout ?? {})[0];
 
   return <RouterProvider value={context}>{children}</RouterProvider>;
 }
