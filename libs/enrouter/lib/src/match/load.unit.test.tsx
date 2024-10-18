f4e85119b8ca610dd610de085c8f55933cb7145e
Eugene Pobochny <eu-ge-ne@users.noreply.github.com>
2024-10-18 01:08:00 -0400

    fix: Match load unit tests (#101)

---
+++ libs/enrouter/lib/src/match/load.unit.test.tsx
@@ -0,0 +1,41 @@
+import { describe, test, expect } from "vitest";
+import * as regexparam from "regexparam";
+
+import type { Match } from "./mod.js";
+import { loadMatches } from "./load.js";
+
+describe("loadMatches", () => {
+  test("0 matches", async () => {
+    const matches: Match[] = [];
+
+    await loadMatches(matches);
+
+    expect(matches).toMatchSnapshot();
+  });
+
+  test("1 match", async () => {
+    const matches: Match[] = [
+      {
+        route: {
+          path: "/",
+          test: regexparam.parse("/", true),
+          modules: [],
+          loaded: true,
+          elements: {
+            layout: {
+              Main: <div>Layout</div>,
+            },
+          },
+        },
+        isFull: true,
+        location: "/",
+        params: {},
+      },
+    ];
+    matches[0]!.fist = matches[0];
+
+    await loadMatches(matches);
+
+    expect(matches).toMatchSnapshot();
+  });
+});
