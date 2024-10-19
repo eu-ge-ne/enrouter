4552ce209d20fa3a52ab40cca43793b69ecde694
Eugene Pobochny <eugene.pobochny@gmail.com>
2024-10-18 22:38:41 -0400

    1

--- libs/enrouter/lib/src/outlet/mod.tsx
+++ libs/enrouter/lib/src/outlet/mod.tsx
@@ -11,16 +11,16 @@ export function Outlet({ name }: OutletProps): ReactNode {
   const match = useMatch();
 
   if (!match.next) {
-    return match.route?.elements?.index?.[name];
+    return match.route?.elements.index?.[name];
   }
 
   if (!match.next.route) {
-    return match.route?.elements?.notFound?.[name] ?? <NotFound />;
+    return match.route?.elements.notFound?.[name] ?? <NotFound />;
   }
 
   return (
     <MatchProvider value={match.next}>
-      {match.next?.route?.elements?.layout?.[name]}
+      {match.next?.route?.elements.layout?.[name]}
     </MatchProvider>
   );
 }
