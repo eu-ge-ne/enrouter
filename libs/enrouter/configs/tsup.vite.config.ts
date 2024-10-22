8a2e0db20876ace12de103eb9bc699402f1b199d
Eugene Pobochny <eugene.pobochny@gmail.com>
2024-10-22 18:32:12 -0400

    fix: vite manifest tests

--- libs/enrouter/configs/tsup.vite.config.ts
+++ libs/enrouter/configs/tsup.vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["vite/src/plugin/mod.ts", "vite/src/manifest.ts"],
+  entry: ["vite/src/plugin/mod.ts", "vite/src/manifest/mod.ts"],
   outDir: "dist/vite",
   format: "esm",
   target: "es2023",
