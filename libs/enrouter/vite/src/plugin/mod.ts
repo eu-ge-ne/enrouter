a71d141721a9cc33bc291371f0f3ac19b319e4ae
Eugene Pobochny <eu-ge-ne@users.noreply.github.com>
2024-10-23 14:35:36 -0400

    fix: vite plugin (#137)

--- libs/enrouter/vite/src/plugin/mod.ts
+++ libs/enrouter/vite/src/plugin/mod.ts
@@ -1,7 +1,7 @@
 import { resolve } from "node:path";
 import { format } from "node:util";
 
-import { type Plugin } from "vite";
+import type { Plugin } from "vite";
 import { glob } from "glob";
 
 import { buildModuleTree } from "./modules.js";
@@ -34,17 +34,16 @@ export default function plugin(params: EnrouterPluginOptions): Plugin {
 
       const routesPath = resolve(params.path);
 
-      this.info(format("Searching modules in %s", routesPath));
+      this.debug(() => format("Searching modules in %s", routesPath));
 
       const files = await glob(resolve(routesPath, "**/_*.tsx"));
       const resolves = await Promise.all(
-        files.map((x) => {
-          return this.resolve(x).then((y) => {
-            if (y) {
-              this.info(format("Found module: %s", y.id));
-            }
-            return y;
-          });
+        files.map(async (x) => {
+          const y = await this.resolve(x);
+          if (y) {
+            this.debug(() => format("Found module: %s", y.id));
+          }
+          return y;
         }),
       );
 
@@ -52,9 +51,9 @@ export default function plugin(params: EnrouterPluginOptions): Plugin {
         .map((x, i) => (x ? { file: files[i]!, resolvedId: x.id } : undefined))
         .filter((x) => x !== undefined);
 
-      this.info(format("Found %d modules", resolvedFiles.length));
+      this.debug(() => format("Found %d modules", resolvedFiles.length));
 
-      this.info("Building module tree");
+      this.debug(() => "Building module tree");
 
       const routeModules = buildModuleTree({
         rootPath,
@@ -62,7 +61,7 @@ export default function plugin(params: EnrouterPluginOptions): Plugin {
         resolvedFiles,
       });
 
-      this.info(format("Module tree: %o", routeModules));
+      this.debug(() => format("Module tree: %o", routeModules));
 
       const compiled = compileRouteTree(routeModules);
 
