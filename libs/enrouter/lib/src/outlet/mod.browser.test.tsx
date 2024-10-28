ea554932ab79b9e26fd1b69c80ffbab5b1f064d4
Eugene Pobochny <eugene.pobochny@gmail.com>
2024-10-28 02:42:50 -0400

    1

--- libs/enrouter/lib/src/outlet/mod.browser.test.tsx
+++ libs/enrouter/lib/src/outlet/mod.browser.test.tsx
@@ -129,6 +129,104 @@ describe("outlet", () => {
     expect(screen.container).toMatchSnapshot();
   });
 
+  test("default _content element from next match", async () => {
+    const match: Match = {
+      isVoid: false,
+      route: {
+        path: "/",
+        test: regexparam.parse("/", true),
+        modules: [],
+        loaded: true,
+        elements: {
+          _layout: <div>layout</div>,
+          _content: <div>content</div>,
+        },
+      },
+      location: "/",
+      isFull: false,
+      params: {},
+
+      next: {
+        isVoid: false,
+        route: {
+          path: "/abc",
+          test: regexparam.parse("/abc", true),
+          modules: [],
+          loaded: true,
+          elements: {
+            _content: <div>next content</div>,
+          },
+        },
+        location: "/abc",
+        isFull: false,
+        params: {},
+      },
+    };
+
+    const screen = render(
+      <MatchProvider value={match}>
+        <Outlet />
+      </MatchProvider>,
+      { wrapper },
+    );
+
+    await expect.element(screen.getByTestId(wrapperId)).toBeVisible();
+
+    expect(screen.container).toMatchSnapshot();
+  });
+
+  test("named _content element from next match", async () => {
+    const match: Match = {
+      isVoid: false,
+      route: {
+        path: "/",
+        test: regexparam.parse("/", true),
+        modules: [],
+        loaded: true,
+        elements: {
+          _layout: {
+            Main: <div>layout#Main</div>,
+          },
+          _content: {
+            Main: <div>content#Main</div>,
+          },
+        },
+      },
+      location: "/",
+      isFull: false,
+      params: {},
+
+      next: {
+        isVoid: false,
+        route: {
+          path: "/abc",
+          test: regexparam.parse("/abc", true),
+          modules: [],
+          loaded: true,
+          elements: {
+            _content: {
+              Main: <div>next content#Main</div>,
+            },
+          },
+        },
+        location: "/abc",
+        isFull: false,
+        params: {},
+      },
+    };
+
+    const screen = render(
+      <MatchProvider value={match}>
+        <Outlet name="Main" />
+      </MatchProvider>,
+      { wrapper },
+    );
+
+    await expect.element(screen.getByTestId(wrapperId)).toBeVisible();
+
+    expect(screen.container).toMatchSnapshot();
+  });
+
   test("default _content element from current match", async () => {
     const match: Match = {
       isVoid: false,
