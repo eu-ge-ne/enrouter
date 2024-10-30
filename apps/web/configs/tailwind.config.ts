b518ddedae1b7c74f12d41cb8e8496718c727c7c
Eugene Pobochny <eugene.pobochny@gmail.com>
2024-10-29 20:52:35 -0400

    1

--- apps/web/configs/tailwind.config.ts
+++ apps/web/configs/tailwind.config.ts
@@ -5,7 +5,10 @@ import type { Config } from "tailwindcss";
 import colors from "tailwindcss/colors";
 
 const content = [
-  resolve(fileURLToPath(new URL(".", import.meta.url)), "../src/**/*.{ts,tsx,mdx}"),
+  resolve(
+    fileURLToPath(new URL(".", import.meta.url)),
+    "../src/**/*.{ts,tsx,mdx}",
+  ),
 ];
 
 export default {
