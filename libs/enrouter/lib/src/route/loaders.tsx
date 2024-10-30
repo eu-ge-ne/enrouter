095188264a919d1ef63a67bf31ad9788ab9455ae
Eugene Pobochny <eugene.pobochny@gmail.com>
2024-10-30 13:38:34 -0400

    1

--- libs/enrouter/lib/src/route/loaders.tsx
+++ libs/enrouter/lib/src/route/loaders.tsx
@@ -2,32 +2,28 @@ import type { ComponentType } from "react";
 
 import type { Route } from "./mod.js";
 
-type ImportAny = () => Promise<{
-  default: ComponentType | Record<string, ComponentType>;
+type ImportFn = () => Promise<{
+  default: Record<string, ComponentType>;
 }>;
 
 type Loader = (route: Route, fn: () => Promise<unknown>) => Promise<void>;
 
 export const loaders: Record<string, Loader> = {
   "_layout.tsx": async ({ elements }, fn) => {
-    elements._layout = await loadAny(fn);
+    elements._layout = await load(fn);
   },
   "_content.tsx": async ({ elements }, fn) => {
-    elements._content = await loadAny(fn);
+    elements._content = await load(fn);
   },
   "_void.tsx": async ({ elements }, fn) => {
-    elements._void = await loadAny(fn);
+    elements._void = await load(fn);
   },
 };
 
-async function loadAny(fn: () => Promise<unknown>) {
-  const { default: Comp } = await (fn as ImportAny)();
-
-  if (typeof Comp === "function") {
-    return <Comp />;
-  }
+async function load(fn: () => Promise<unknown>) {
+  const { default: Components } = await (fn as ImportFn)();
 
   return Object.fromEntries(
-    Object.entries(Comp).map(([key, C]) => [key, <C />]),
+    Object.entries(Components).map(([key, C]) => [key, <C />]),
   );
 }
