72c354fba43f3a0db41eddb6d3d9f924cdfac7b7
Eugene Pobochny <eu-ge-ne@users.noreply.github.com>
2024-11-01 17:20:55 -0400

    chore: src (#161)

--- configs/tsup.config.lib.ts
+++ configs/tsup.config.lib.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from "tsup";
+import type { Options } from "tsup";
 
-export default defineConfig({
-  entry: ["lib/src/mod.ts"],
+export default {
+  entry: ["src/lib/mod.ts"],
   outDir: "dist/lib",
   format: "esm",
   target: "es2023",
   dts: true,
   external: ["react", "virtual:enrouter"],
   treeshake: true,
-});
+} satisfies Options;
