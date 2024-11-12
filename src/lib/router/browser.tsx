0aa680a8a415a48a5569b3da30444f000fd5d6d0
Eugene Pobochny <eugene.pobochny@gmail.com>
2024-11-11 22:27:44 -0500

    1

--- src/lib/router/browser.tsx
+++ src/lib/router/browser.tsx
@@ -6,6 +6,7 @@ import { matchLocation } from "#lib/match/location.js";
 import { NavigateProvider } from "#lib/navigate/mod.js";
 import { LocationProvider } from "#lib/location/mod.js";
 import { MatchProvider } from "#lib/match/context.js";
+import * as browser from "#lib/browser/mod.js";
 import { Root } from "./root.js";
 
 const log = logger("router/browser");
@@ -19,12 +20,20 @@ export function BrowserRouter(props: BrowserRouterProps): ReactNode {
   const [match, setMatch] = useState(props.match);
 
   const navigate = useCallback(async (to: string) => {
-    window.history.pushState({}, "", to);
-    setLocation(to);
+    try {
+      const nextMacth = await matchLocation(to);
 
-    setMatch(await matchLocation(to));
+      browser.pushHistory(to);
+      setLocation(to);
 
-    log("Navigated to %s", to);
+      setMatch(nextMacth);
+
+      log("Navigated to %s", to);
+    } catch (err) {
+      log("Navigation error %o", err);
+
+      browser.assignLocation(to);
+    }
   }, []);
 
   const handlePopState = useCallback(async (e: PopStateEvent) => {
--- src/lib/router/link.browser.test.tsx
+++ src/lib/router/link.browser.test.tsx
@@ -1,5 +1,5 @@
 import type { FC, PropsWithChildren } from "react";
-import { describe, test, expect, vi } from "vitest";
+import { describe, test, expect, vi, beforeEach } from "vitest";
 import { render } from "vitest-browser-react";
 import { userEvent } from "@vitest/browser/context";
 import * as regexparam from "regexparam";
@@ -7,9 +7,11 @@ import * as regexparam from "regexparam";
 import type { Route } from "#lib/route/mod.js";
 import type { Match } from "#lib/match/mod.js";
 import { getRouteTree } from "#lib/route/tree.js";
+import { matchLocation } from "#lib/match/location.js";
 import { useLink } from "#lib/link/mod.js";
 import { Outlet } from "#lib/outlet/mod.js";
 import { BrowserRouter } from "./browser.js";
+import { assignLocation, pushHistory } from "#lib/browser/mod.js";
 
 const wrapperId = "test-wrapper";
 
@@ -20,6 +22,17 @@ const wrapper: FC<PropsWithChildren> = ({ children }) => (
 vi.mock(import("#lib/route/tree.js"), () => ({
   getRouteTree: vi.fn(),
 }));
+
+vi.mock(import("#lib/match/location.js"), { spy: true });
+
+vi.mock(import("#lib/browser/mod.js"), () => ({
+  pushHistory: vi.fn(),
+  assignLocation: vi.fn(),
+}));
+
+beforeEach(() => {
+  vi.clearAllMocks();
+});
 
 describe("router", () => {
   describe("BrowserRouter", () => {
@@ -75,10 +88,79 @@ describe("router", () => {
       const screen = render(<BrowserRouter match={match} />, { wrapper });
 
       await userEvent.click(screen.getByRole("link"));
-
       await expect.element(screen.getByTestId(wrapperId)).toBeVisible();
 
       expect(screen.container).toMatchSnapshot();
+
+      expect(pushHistory).toBeCalledTimes(1);
+      expect(pushHistory).toBeCalledWith("/abc");
+
+      expect(assignLocation).not.toBeCalled();
+    });
+
+    test("matchLocation fails", async () => {
+      function TestLink() {
+        const props = useLink("/abc");
+        return <a {...props}>link to /abc</a>;
+      }
+
+      const route: Route = {
+        path: "/",
+        test: regexparam.parse("/", true),
+        modules: [],
+        loaded: true,
+        elements: {
+          _layout: {
+            Root: (
+              <div>
+                <TestLink />
+                <Outlet />
+              </div>
+            ),
+          },
+        },
+        tree: [
+          {
+            path: "/abc",
+            test: regexparam.parse("/abc", true),
+            modules: [],
+            loaded: true,
+            elements: {
+              _layout: {
+                Abc: <div>abc</div>,
+              },
+            },
+          },
+        ],
+      };
+
+      vi.mocked(getRouteTree).mockReturnValueOnce(route);
+      vi.mocked(matchLocation).mockRejectedValueOnce(
+        new Error("matchLocation error"),
+      );
+
+      const match: Match = {
+        isVoid: false,
+        route,
+        location: "/",
+        isExact: true,
+        params: {},
+      };
+
+      match.first = match;
+      match.last = match;
+
+      const screen = render(<BrowserRouter match={match} />, { wrapper });
+
+      await userEvent.click(screen.getByRole("link"));
+      await expect.element(screen.getByTestId(wrapperId)).toBeVisible();
+
+      expect(screen.container).toMatchSnapshot();
+
+      expect(pushHistory).not.toBeCalled();
+
+      expect(assignLocation).toBeCalledTimes(1);
+      expect(assignLocation).toBeCalledWith("/abc");
     });
   });
 });
