164fd6846e7856465bd923b6568e471e7c1413c2
Eugene Pobochny <eu-ge-ne@users.noreply.github.com>
2024-12-20 21:47:00 -0500

    fix: useLocation (#185)

--- src/lib/mod.ts
+++ src/lib/mod.ts
@@ -10,7 +10,7 @@ export { type UseActiveParams, useActive } from "./match/useActive.js";
 export { type StaticRouterProps, StaticRouter } from "./router/static.js";
 export { type BrowserRouterProps, BrowserRouter } from "./router/browser.js";
 
-export { useLocation } from "./location/mod.js";
+export { useLocation } from "./location/location.js";
 export { useNavigate } from "./navigate/mod.js";
 export { type OutletProps, Outlet } from "./outlet/outlet.js";
 export { type LinkProps, useLink } from "./link/mod.js";
